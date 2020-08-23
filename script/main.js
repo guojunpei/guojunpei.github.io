8653057649c916649202a208e302418afd752118
guojunpei <gjp599@163.com>
2020-08-24 00:51:34 +0800

    js add

--- script/main.js
+++ script/main.js
@@ -6,8 +6,10 @@ myImage.onclick = function(){
     let mySrc = myImage.getAttribute('src');
     if(mySrc.endsWith('image/wx.jpg')){
       myImage.setAttribute('src', 'image/wxewm.jpg');
+      myHeading.style.color='red';
     } else {
       myImage.setAttribute('src', 'image/wx.jpg');
+      myHeading.style.color='white';
     }
 }
 myImage.style.cursor='pointer'
