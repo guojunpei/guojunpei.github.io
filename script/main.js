72d7fe5cc12dd202888714cd6f8382070d9a6e6e
guojunpei <gjp599@163.com>
2020-08-23 22:35:40 +0800

    add charging image by js

--- script/main.js
+++ script/main.js
@@ -1,2 +1,12 @@
 let myHeading = document.querySelector('h6');
-myHeading.textContent = 'Hello world!'
+myHeading.textContent = 'Hello world!';
+
+let myImage = document.querySelector("img");
+myImage.onclock = function(){
+    let mySrc = myImage.getAttribute("src");
+    if(mySrc === 'image/wx.jpg'){
+      myImage.setAttribute('src', 'image/wxewm.jpg');
+    } else {
+      myImage.setAttribute('src', 'imge/wxjpg');
+    }
+}
