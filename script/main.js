7eb9e2de62ee84080607c9441345599dc5f0408c
guojunpei <gjp599@163.com>
2020-09-17 01:06:53 +0800

    fix

--- script/main.js
+++ script/main.js
@@ -156,12 +156,20 @@ function hanoi(n,a,b,c){
   if(n===1){console.log(a,"-->",c);}
   else{
     hanoi(n-1,a,c,b);
-    //hanoi(n-1,a,b,c);
-    //hanoi(n-1,b,a,c);
+    hanoi(1,a,b,c);
+    hanoi(n-1,b,a,c);
   }
-
 }
 
+let nr=0
+function hanoi(n,a,b,c){
+  if(n===1){console.log(a,"-->",c,nr++);}
+  else{
+    hanoi(n-1,a,c,b);
+    hanoi(1,a,b,c);
+    hanoi(n-1,b,a,c);
+  }
+}
 
 
     function nuo(x,y){
