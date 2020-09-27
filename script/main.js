9cc052000f643a999e83dba92fa2e3285fe1c25b
guojunpei <gjp599@163.com>
2020-09-28 01:36:44 +0800

    fix but still not work .

--- script/main.js
+++ script/main.js
@@ -1262,44 +1262,83 @@ getScore()
 */
 
 //"井字游戏"
-let p1="";
-let p2="";
+
+let p1=" ";
+let p2=" ";
 let p1n="play1's name";
 let p2n="play2's name";
-let zt="  What's your name?  Please player 1 type 'name(yourname)'.";
-let a1=" ";
-let a2=" ";
-let a3=" ";
-let b1=" ";
-let b2=" ";
-let b3=" ";
-let c1=" ";
-let c2=" ";
-let c3=" ";
-let qipan= "╔═══╦═══╦═══╗\n║ "+a1+" ║ "+a2+" ║ "+a3+" ║"+"  x:"+p1n+"\n╠═══╬═══╬═══╣\n║ "+b1+" ║ "+b2+" ║ "+b3+" ║"+"  o:"+p2n+"\n╠═══╬═══╬═══╣\n║ "+c1+" ║ "+c2+" ║ "+c3+" ║"+zt+"\n╚═══╩═══╩═══╝";
+let zt="  What's your name?  Please player 1 type 'name('yourname')'.";
+let a1="  ";
+let a2="  ";
+let a3="  ";
+let b1="  ";
+let b2="  ";
+let b3="  ";
+let c1="  ";
+let c2="  ";
+let c3="  ";
+let qipan= "╔═══╦═══╦═══╗\n║ "+a1+"║ "+a2+"║ "+a3+"║"+"  x:"+p1n+"\n╠═══╬═══╬═══╣\n║ "+b1+"║ "+b2+"║ "+b3+"║"+"  o:"+p2n+"\n╠═══╬═══╬═══╣\n║ "+c1+"║ "+c2+"║ "+c3+"║"+zt+"\n╚═══╩═══╩═══╝";
 
-function again(){
-  let a1=" ";
-  let a2=" ";
-  let a3=" ";
-  let b1=" ";
-  let b2=" ";
-  let b3=" ";
-  let c1=" ";
-  let c2=" ";
-  let c3=" ";
-  let zt="  What's your name?  Please player 1 type 'name('yourname')'.";
+function game(){
   console.log(qipan);
 }
 
 function name(n){
-  if(p1===""){p1n=n;p1=n;zt="  Welcome~"+n+"! What's your name, player 2 ? Please player 2 type 'name('yourname')'.";}else{p2n=n;p2=n;zt="  Welcome~"+n+"! Please "+p1+"start the game with type 'luozi('p1' or play's name,weizhi)'";}
+  if(p1===" "){
+    p1n=n;
+    p1=n;
+    zt="  Welcome~"+n+"! What's your name, player 2 ? Please player 2 type 'name('yourname')'.";
+  }
+  else{
+    p2n=n;
+    p2=n;
+    zt="  Welcome~"+n+"! Please "+p1+"start the game with type 'luozi('p1' or play's name,weizhi like 'a1')'";
+    let a1="a1";
+    let a2="a2";
+    let a3="a3";
+    let b1="b1";
+    let b2="b2";
+    let b3="b3";
+    let c1="c1";
+    let c2="c2";
+    let c3="c3";
+  }
   console.log(qipan);
 }
 
+function again(){
+  let a1="a1";
+  let a2="a2";
+  let a3="a3";
+  let b1="b1";
+  let b2="b2";
+  let b3="b3";
+  let c1="c1";
+  let c2="c2";
+  let c3="c3";
+  let zt="  Please "+p1+"start the game with type 'luozi('p1' or play's name,weizhi like 'a1')'";
+  console.log(qipan);
+}
+
+function exchange(){
+  let p0=p2;
+  let p2=p1;
+  let p1=p0;
+  let a1="a1";
+  let a2="a2";
+  let a3="a3";
+  let b1="b1";
+  let b2="b2";
+  let b3="b3";
+  let c1="c1";
+  let c2="c2";
+  let c3="c3";
+  let zt="  Please "+p1+"start the game with type 'luozi('p1' or play's name,weizhi like 'a1')'";
+}
+
 function luozi(p,q){
-  if(p===p1){qizi="x";zt="  x"+q+"; o 's time! Please "+p2+" type 'luozi('p1' or play's name,weizhi)'";}
-  if(p===p2){qizi="o";zt="  x"+o+"; x 's time! Please "+p1+" type 'luozi('p1' or play's name,weizhi)'";}
+  if(p===p1){qizi="x ";zt="  x"+q+"; o 's time! Please "+p2+" type 'luozi('p1' or play's name,weizhi like 'a1')'";}
+  if(p===p2){qizi="o ";zt="  x"+o+"; x 's time! Please "+p1+" type 'luozi('p1' or play's name,weizhi like 'a1')'";}
   if(q===a1){a1=qizi;}
   if(q===a2){a1=qizi;}
   if(q===a3){a1=qizi;}
@@ -1309,14 +1348,14 @@ function luozi(p,q){
   if(q===c1){c1=qizi;}
   if(q===c2){c2=qizi;}
   if(q===c3){c3=qizi;}
-  if(a1===a2 && a1===a3 && a1!==" "){zt="  Game over! Winer is "+p+"! One more? Please tpye 'again()'";}
-  if(b1===b2 && b1===b3 && b1!==" "){zt="  Game over! Winer is "+p+"! One more? Please tpye 'again()'";}
-  if(c1===c2 && c1===c3 && c1!==" "){zt="  Game over! Winer is "+p+"! One more? Please tpye 'again()'";}
-  if(a1===b1 && a1===c1 && a1!==" "){zt="  Game over! Winer is "+p+"! One more? Please tpye 'again()'";}
-  if(a2===b2 && a2===c2 && a2!==" "){zt="  Game over! Winer is "+p+"! One more? Please tpye 'again()'";}
-  if(a3===b3 && a3===c3 && a3!==" "){zt="  Game over! Winer is "+p+"! One more? Please tpye 'again()'";}
-  if(a1===b2 && a1===c3 && a1!==" "){zt="  Game over! Winer is "+p+"! One more? Please tpye 'again()'";}
-  if(a3===b2 && a3===c1 && a3!==" "){zt="  Game over! Winer is "+p+"! One more? Please tpye 'again()'";}
+  if(a1===a2 && a1===a3 ){zt="  Game over! Winer is "+p+"! One more? Please tpye 'again()'! Exchange and again? Please tpye 'exchange()'!";}
+  if(b1===b2 && b1===b3 ){zt="  Game over! Winer is "+p+"! One more? Please tpye 'again()'! Exchange and again? Please tpye 'exchange()'!";}
+  if(c1===c2 && c1===c3 ){zt="  Game over! Winer is "+p+"! One more? Please tpye 'again()'! Exchange and again? Please tpye 'exchange()'!";}
+  if(a1===b1 && a1===c1 ){zt="  Game over! Winer is "+p+"! One more? Please tpye 'again()'! Exchange and again? Please tpye 'exchange()'!";}
+  if(a2===b2 && a2===c2 ){zt="  Game over! Winer is "+p+"! One more? Please tpye 'again()'! Exchange and again? Please tpye 'exchange()'!";}
+  if(a3===b3 && a3===c3 ){zt="  Game over! Winer is "+p+"! One more? Please tpye 'again()'! Exchange and again? Please tpye 'exchange()'!";}
+  if(a1===b2 && a1===c3 ){zt="  Game over! Winer is "+p+"! One more? Please tpye 'again()'! Exchange and again? Please tpye 'exchange()'!";}
+  if(a3===b2 && a3===c1 ){zt="  Game over! Winer is "+p+"! One more? Please tpye 'again()'! Exchange and again? Please tpye 'exchange()'!";}
   console.log(qipan);
 }
 /*
