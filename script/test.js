433291eff325a939663f7515483db79529dc13ef
guojunpei <gjp599@163.com>
2020-10-31 22:42:22 +0800

    practice input click and mouse over

--- script/test.js
+++ script/test.js
@@ -1,9 +1,10 @@
 
-function mouseOver(){
-  console.log("The chekbox1 has been mouse over "+new Date());
+function buttonClick(Obj){
+  console.log(Obj.id+" has been click "+new Date());
 }
-  
-function click(){
-  console.log("The chekbox1 has been click "+new Date());
-}
-  
+
+
+function buttonMouseOver(Obj){
+    console.log(Obj.id+ " has been mouse over "+new Date());
+  }
+    
