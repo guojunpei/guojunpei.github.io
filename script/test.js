bdd3710cda4be32d7af7b4e5724fc694876280ee
guojunpei <gjp599@163.com>
2020-11-01 23:24:17 +0800

    fix

--- script/test.js
+++ script/test.js
@@ -2,11 +2,11 @@
 function btnClickHandler(Obj){
     btntext1.textContent=Obj.id;
     btntext1.style.color=Obj.style.color;
-    btntext2.textContent+=Obj.id+" : click;\n";
+    btntext2.textContent+=Obj.id+" : click;";
 }
 
 function btnMouseOverHandler(Obj){
     btntext1.textContent=Obj.id;
     btntext1.style.color=Obj.style.color;
-    btntext2.textContent+=Obj.id+" : mouse over;\n";
+    btntext2.textContent+=Obj.id+" : mouse over;";
 }
