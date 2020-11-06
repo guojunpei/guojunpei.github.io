9a200e0c570061a5bf28a0cd72ff3602a8a9c638
guojunpei <gjp599@163.com>
2020-11-07 01:18:56 +0800

    fix and reset the document

--- pages/responsive/js/app.js
+++ pages/responsive/js/app.js
@@ -1,12 +1,55 @@
-document.querySelector("#app").innerHTML=`
-<div class="my-body">1</div>
-`;
+const appNode = document.querySelector("#app");
 
-function width1(){
-    document.querySelector("#app").innerHTML=`
-        <div class="my-body">2</div>
+function uiMobile () {
+    appNode.innerHTML=`
+    <div class="my-body">1</div>
     `;
-
+    appNode.style.color = "red";
 }
 
-window.addEventListener("resize",);
+function uiSm(){
+    appNode.innerHTML=`
+        <div class="my-body">small</div>
+    `;
+    console.log("small");
+    appNode.style.color = "green";
+}
+
+function uiLg(){
+    appNode.innerHTML=`
+        <div class="my-body">large</div>
+    `;
+    console.log("large");
+    appNode.style.color = "blue";
+}
+
+function windowResizeHandler (event) {
+    const width = window.innerWidth;
+    if(width > 500){
+        uiLg();
+    }else if(width > 300){
+        uiSm();
+    }else {
+        uiMobile();
+    }
+}
+
+windowResizeHandler();
+window.addEventListener("resize", windowResizeHandler);
+
+// console:
+a = null;
+b = undefined;
+c = 0;
+d = "";
+
+a == b; // true
+a === b; // false
+
+c == d; // true
+c === d; // false
+
+false == c; // true
+false === c; // false
+false === false; // true
+false === true; // false
