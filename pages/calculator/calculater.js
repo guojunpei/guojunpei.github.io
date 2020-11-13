6455bd10695ef77940705531c5b29e318cc6524b
guojunpei <gjp599@163.com>
2020-11-13 17:12:18 +0800

    fix

--- pages/calculator/calculater.js
+++ pages/calculator/calculater.js
@@ -6,8 +6,12 @@ let symbol="";
 let result=0;
 
 function input(n){
-    inputRsult=inputRsult+n;
-    number0=number0.toString+n;
+    if(inputRsult===null){
+        inputRsult=n;
+    }else{
+        inputRsult=inputRsult+n.toString();
+    }
+    number0=Number(Number(number0)+n.toString());
     document.getElementById("input-result").textContent=inputRsult;
 }
 
@@ -29,7 +33,7 @@ function outEqual(){
     inputRsult=inputRsult+"=";
     number2=Number(number0);
     if(symbol===""&&number1===null){
-        result=number0;
+        result=Number(number0);
     }else if(symbol==="+"){
         result=number1+number2;
     }else if(symbol==="-"){
@@ -53,30 +57,40 @@ function percent(){
 }
 
 function allClear(){
-    inputRsult="0";
+    inputRsult=null;
     number0=null;
     number1=null;
     number2=null;
     symbol="";
     result=0;
     document.getElementById("calculate-result").textContent=result;
-    document.getElementById("input-result").textContent=inputRsult;
+    if(inputRsult===null){
+        document.getElementById("input-result").textContent=0;
+    }else{
+        document.getElementById("input-result").textContent=inputRsult;
+    }
 }
 
-function clear(){
-    inputRsult=inputRsult.substring(0,inputRsult.length-1);
+function deleteEnd(s){
+    return s.substring(0,s.length-1);
+}
+
+function oneClear(){
+    function deleteEnd(s){
+        return s.substring(0,s.length-1);
+    }
+    inputRsult=deleteEnd(inputRsult);
+    document.getElementById("input-result").textContent=inputRsult;
     if(symbol===""&&number0===null&&number1!==null){
-        result=result+" clear lose! "
+        document.getElementById("one-clear").style.color=red;
     }else if(symbol!==""&&number0!==null&&number1!==null){
-        number2=number2.substring(0,number2.length-1);
+        number2=deleteEnd(number2);
     }else if(symbol!==""&&number0===null&&number1!==null){
         symbol="";
         number0=number1;
         number1=null;
     }else{
-        number0=number0.substring(0,number0.length-1);
-    }
-    document.getElementById("calculate-result").textContent=result;
-    document.getElementById("input-result").textContent=inputRsult;
+        number0=deleteEnd(number0);
+    }   
 }
 
