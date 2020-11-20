bc98d0c3e45d515639b3ef334b526443fd0dee27
guojunpei <gjp599@163.com>
2020-11-20 16:40:58 +0800

    create web-app-tic-tac-toe-game

---
+++ pages/tic-tac-toe-game/tic-tac-toe-game.js
@@ -0,0 +1,14 @@
+import "./tic-tac-toe-game.css";
+
+let app=document.getElementById("app-tic-tac-toe-game");
+app.style.display="flex";
+app.style.justifyContent="center";
+app.style.alignItems="center";
+app.style.height="100%";
+app.style.width="100%";
+app.style.padding="0";
+app.style.margin="0";
+app.style.backgroundColor="black";
+
+
+
