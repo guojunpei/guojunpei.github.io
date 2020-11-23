3216509aa4bd8d66ed446001688760e5c8e54c9b
guojunpei <gjp599@163.com>
2020-11-23 22:00:14 +0800

    add

--- pages/tic-tac-toe-game/tic-tac-toe-game.js
+++ pages/tic-tac-toe-game/tic-tac-toe-game.js
@@ -17,5 +17,14 @@ let row=document.createElement("div");
 
 let btn=document.createElement("button");
 
+let rowSize=3;
+let columnSize=3;
+
+for(let i=0;i<rowSize;i++){
+    for(let j=0;j<columnSize;j++){
+        btn;
+        btn.innerText=i*columnSize+j+1;
+    }
+}
 
 
