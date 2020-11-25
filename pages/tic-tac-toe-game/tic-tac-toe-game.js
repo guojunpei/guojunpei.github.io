a39553a5ed688d076ed55d6279f30e908572d1e8
guojunpei <gjp599@163.com>
2020-11-25 16:24:51 +0800

    fix

--- pages/tic-tac-toe-game/tic-tac-toe-game.js
+++ pages/tic-tac-toe-game/tic-tac-toe-game.js
@@ -1,51 +1,5 @@
 //import "./tic-tac-toe-game.css";
 
-/*
-const app = document.getElementById("app-tic-tac-toe-game");
-
-app.style.display="flex";
-app.style.justifyContent="center";
-app.style.alignItems="center";
-app.style.height="100%";
-app.style.width="100%";
-app.style.padding="0";
-app.style.margin="0";
-app.style.backgroundColor="black";
-
-const rowSize = 3;
-const colSize = 4;
-const btnWidth = 80;
-const btnMargin = 2;
-const containerPadding = 10;
-
-const container = document.createElement("div");
-container.style.backgroundColor = "#212F3D";
-container.style.padding = `${containerPadding}px`;
-container.style.width = `${rowSize * (btnWidth + btnMargin * 2)}px`;
-
-for (let i = 0; i < colSize; i++) {
-  const row = document.createElement("div");
-
-  for (let j = 0; j < rowSize; j++) {
-    const btn = document.createElement("button");
-    btn.innerText = j + colSize * i;
-    const s = btn.style;
-    s.margin = `${btnMargin}px`;
-    s.width = `${btnWidth}px`;
-    s.height = "50px";
-    s.backgroundColor = "#566573";
-    s.color = "#EB984E";
-    s.fontSize = "20px";
-    s.borderStyle = "none";
-    row.appendChild(btn);
-  }
-  container.appendChild(row);
-}
-
-app.appendChild(container);
-*/
-
-
 let app=document.getElementById("app-tic-tac-toe-game");
 app.style.display="block";
 app.style.justifyContent="center";
@@ -66,7 +20,8 @@ let gs=gameContainer.style;
 gs.backgroundColor="black";
 gs.width=`${columnSize*btnWidth}rem`;
 gs.height=`${rowSize*btnHeight}rem`;
-gs.padding="1rem"
+gs.padding="1rem";
+gs.border="1rem solid purple";
 
 for(let i=0;i<rowSize;i++){
     let row=document.createElement("div");
@@ -77,7 +32,7 @@ for(let i=0;i<rowSize;i++){
         btn.innerText=i*columnSize+j+1;
         let bs=btn.style;
         bs.width=`${btnWidth}rem`;
-        bs.height==`${btnHeight}rem`;
+        bs.height=`${btnHeight}rem`;
         bs.padding="0";
         bs.margin="0";
         bs.color="white";
@@ -85,26 +40,9 @@ for(let i=0;i<rowSize;i++){
         bs.borderWidth="0.1rem";
         bs.borderColor="gray";
         row.appendChild(btn);
-        //bs.borderStyle="none";
+        bs.borderStyle="solid";
     }
     gameContainer.appendChild(row);
 }
 
 app.appendChild(gameContainer);
-
-/*
-btn.innerText=1;
-bs.width=`${btnWidth}rem`;
-bs.height==`${btnHeight}rem`;
-row.appendChild(btn);
-gameContainer.appendChild(row);
-
-btn.innerText=2;
-row.appendChild(btn);
-gameContainer.appendChild(row);
-app.appendChild(gameContainer);
-*/
-
-
-
-
