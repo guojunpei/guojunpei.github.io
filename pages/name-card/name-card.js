83e33438d9cff580d498393b6bf0aed1aa71b52f
guojunpei <gjp599@163.com>
2020-11-28 00:02:15 +0800

    add eventlistener

--- pages/name-card/name-card.js
+++ pages/name-card/name-card.js
@@ -1,5 +1,13 @@
 let formMain=document.querySelector("form");
 formMain.addEventListener("submit",(e)=>{
     new FormData(formMain);
-    
+});
+
+formMain.addEventListener("formdata",(e)=>{
+    console.log("formdata fired");
+    let data=e.FormData;
+    for(let v of data.values()){
+        console.log(v);
+    }
+        
 })
