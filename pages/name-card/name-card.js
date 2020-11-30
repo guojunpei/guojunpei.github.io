9eb18f63df60adef4326371cb5dc223d76f426a5
guojunpei <gjp599@163.com>
2020-11-30 23:01:22 +0800

    name card can be use!

--- pages/name-card/name-card.js
+++ pages/name-card/name-card.js
@@ -1,7 +1,8 @@
-
+/*
 let formMain=document.querySelector("form");
 
 formMain.addEventListener("submit",(e)=>{
+    e.preventDefault();
     new FormData(formMain);
 });
 
@@ -13,13 +14,24 @@ formMain.addEventListener("formdata",(e)=>{
     }
     document.getElementById("show-name").innerText=data.value;
 })
-
-
-/*
-
-document.querySelector("form").addEventListener("submit",()=>{
-    document.getElementById("show-name").innerText=document.getElementById("name").value;
-})
 */
+
+
+document.querySelector("form").addEventListener("submit",(e)=>{
+    e.preventDefault();
+    document.getElementById("show-name").innerText=document.getElementById("name").value;
+    document.getElementById("show-name").style.color=document.getElementById("color").value;
+    document.getElementById("show-name").style.backgroundColor=document.getElementById("backgroundcolor").value;
+})
+
+document.getElementById("full-screen").addEventListener("click",()=>{
+    document.getElementById("show-name").innerText=document.getElementById("name").value;
+    document.getElementById("show-name").style.color=document.getElementById("color").value;
+    document.getElementById("show-name").style.backgroundColor=document.getElementById("backgroundcolor").value;
+    document.querySelector("form").style.display="none";
+    document.getElementById("show-name").style.fontSize=`${window.innerHeight/2}px`;
+})
+
+
 
 //document.getElementById("preview").addEventListener("click",document.getElementById("show-name").innerText=document.getElementById("name").value);
