66bf2bda62afa71dcc9038668d7d8e27b2413ff1
guojunpei <gjp599@163.com>
2020-11-30 21:48:10 +0800

    fix

--- pages/practice/form/form.js
+++ pages/practice/form/form.js
@@ -1,14 +1,16 @@
-let pwd2=document.getElementById("repwd").value;
-let pwd2BC=document.getElementById("repwd").style.borderColor;
-let pwd1=document.getElementById("pwd").value;
-//let warm=;
+
 
 function testSame(){
+    let pwd2=document.getElementById("repwd").value;
+    let pwd1=document.getElementById("pwd").value;
+    let pwd2BC=document.getElementById("repwd").style.border;
+    //let warm=;
+
     if(pwd1===pwd2){
-        pwd2BC="green";
+        pwd2BC="0.1rem solid green";
     }else{
         //warm="repeat password must be same twice!";
-        pwd2BC="red";
+        pwd2BC="0.1rem dashed red";
     }
 }
 
