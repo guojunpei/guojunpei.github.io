6bdcc522e2de708b394215faf2611de40d48b315
guojunpei <gjp599@163.com>
2020-12-01 01:07:26 +0800

    fix

--- pages/name-card/name-card.js
+++ pages/name-card/name-card.js
@@ -4,18 +4,18 @@ let formMain=document.querySelector("form");
 formMain.addEventListener("submit",(e)=>{
     e.preventDefault();
     new FormData(formMain);
+    //console.log(e);
+    //console.log(new FormData(formMain));
 });
 
 formMain.addEventListener("formdata",(e)=>{
-    console.log("formdata fired");
-    let data=e.FormData;
-    for(let value of data.values()){
-        console.log(value);
-    }
-    document.getElementById("show-name").innerText=data.value;
+    //console.log("formdata fired");
+    const obj = Object.fromEntries(e.formData);
+    //console.log(obj);
+    document.getElementById("show-name").innerText = obj["name"];
 })
-*/
 
+*/
 
 document.querySelector("form").addEventListener("submit",(e)=>{
     e.preventDefault();
@@ -31,7 +31,6 @@ document.getElementById("full-screen").addEventListener("click",()=>{
     document.querySelector("form").style.display="none";
     document.getElementById("show-name").style.fontSize=`${window.innerHeight/2}px`;
 })
-
 
 
 //document.getElementById("preview").addEventListener("click",document.getElementById("show-name").innerText=document.getElementById("name").value);
