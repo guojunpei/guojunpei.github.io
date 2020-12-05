9e7dd3125b85bd3fd6076b538d591fbfe62c9bba
guojunpei <gjp599@163.com>
2020-12-05 22:25:27 +0800

    fix

--- pages/practice/form/form.js
+++ pages/practice/form/form.js
@@ -1,10 +1,9 @@
 
-
-function testSame(e){
+function testSame(){
+    console.log("testSame work!")
     let pwd2=document.getElementById("repwd").value;
     let pwd1=document.getElementById("pwd").value;
     let pwd2BC=document.getElementById("repwd").style.border;
-    e.preventDefault();
 
     if(pwd1===pwd2){
         pwd2BC="0.1rem solid green";
@@ -13,5 +12,20 @@ function testSame(e){
     }
 }
 
-document.querySelector("form").addEventListener("submit",testSame(e));
-//document.getElementById("repwd").addEventListener("focusout",testSame());
+document.getElementById("repwd").addEventListener('focusout',testSame());
+
+let form=document.querySelector("form");
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    new FormData(form);
+    console.log(e);
+});
+
+form.addEventListener("formdata",(e)=>{
+    const obj = Object.fromEntries(e.formData);
+    console.log(obj);
+    let content="";
+    for(let [k,v] of obj){content=content+k+":"+v+";";}
+    document.getElementById("show-form-data").innerText = content;
+});
