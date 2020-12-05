fdf25a12f51b9ac0a79d2d5f1ce593a75b5b7613
guojunpei <gjp599@163.com>
2020-12-05 20:58:34 +0800

    fix form

--- pages/practice/form/form.js
+++ pages/practice/form/form.js
@@ -4,13 +4,11 @@ function testSame(e){
     let pwd2=document.getElementById("repwd").value;
     let pwd1=document.getElementById("pwd").value;
     let pwd2BC=document.getElementById("repwd").style.border;
-    //let warm=;
     e.preventDefault();
 
     if(pwd1===pwd2){
         pwd2BC="0.1rem solid green";
     }else{
-        //warm="repeat password must be same twice!";
         pwd2BC="0.1rem dashed red";
     }
 }
