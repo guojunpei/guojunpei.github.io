68686f193292d9dd7c530b0476f4806104404f9d
guojunpei <gjp599@163.com>
2021-01-02 00:30:30 +0800

    fix

--- pages/practice/fetch/fetch.js
+++ pages/practice/fetch/fetch.js
@@ -1,10 +1,6 @@
 let baseTime=1000;
 let randomTime=500;
 
-const container1=document.getElementById("one-by-one");
-const container2=document.getElementById("after-all");
-const container3=document.getElementById("faster-first");
-
 const filesPath="https://guojunpei.github.io/pages/practice/fetch/";
 
 document.querySelector("button").addEventListener("click",function(){
@@ -13,67 +9,88 @@ document.querySelector("button").addEventListener("click",function(){
     }
     
     function oneByOne(){
-        let t=container1.innerText;
+        let containner=document.getElementById("one-by-one");
         let n=0;
         window.setTimeout(function(){
-            t+="begin...";
+            containner.innerText+="begin...";
         },Math.random()*randomTime+baseTime);
         window.setTimeout(function(){
             n+=1;
-            getFile(n).then(text=>{t+=text;});
+            getFile(n).then(text=>{containner.innerText+=text;});
         },Math.random()*randomTime+baseTime);
         window.setTimeout(function(){
             n+=1;
-            getFile(n).then(text=>{t+=text;});
+            getFile(n).then(text=>{containner.innerText+=text;});
         },Math.random()*randomTime+baseTime);
         window.setTimeout(function(){
             n+=1;
-            getFile(n).then(text=>{t+=text;});
+            getFile(n).then(text=>{containner.innerText+=text;});
         },Math.random()*randomTime+baseTime);
         window.setTimeout(function(){
             n+=1;
-            getFile(n).then(text=>{t+=text;});
+            getFile(n).then(text=>{containner.innerText+=text;});
         },Math.random()*randomTime+baseTime);
         window.setTimeout(function(){
             n+=1;
-            getFile(n).then(text=>{t+=text;});
+            getFile(n).then(text=>{containner.innerText+=text;});
         },Math.random()*randomTime+baseTime);
         window.setTimeout(function(){
             n+=1;
-            getFile(n).then(text=>{t+=text;});
+            getFile(n).then(text=>{containner.innerText+=text;});
         },Math.random()*randomTime+baseTime);
         window.setTimeout(function(){
             n+=1;
-            getFile(n).then(text=>{t+=text;});
+            getFile(n).then(text=>{containner.innerText+=text;});
         },Math.random()*randomTime+baseTime);
         window.setTimeout(function(){
             n+=1;
-            getFile(n).then(text=>{t+=text;});
+            getFile(n).then(text=>{containner.innerText+=text;});
         },Math.random()*randomTime+baseTime);
     }
     
     function afterAll(){
-        let t=container2.innerText;
+        let containner=document.getElementById("after-all");
         window.setTimeout(function(){
-            t+="begin...";
+            containner.innerText+="begin...";
         },Math.random()*randomTime+baseTime);
+
+        let t1="";
+        let t2="";
+        let t3="";
+        let t4="";
+        let t5="";
+        let t6="";
+        let t7="";
+        let t8="";
+        getFile(1).then(text=>{t1=text;});
+        getFile(2).then(text=>{t2=text;});
+        getFile(3).then(text=>{t3=text;});
+        getFile(4).then(text=>{t4=text;});
+        getFile(5).then(text=>{t5=text;});
+        getFile(6).then(text=>{t6=text;});
+        getFile(7).then(text=>{t7=text;});
+        getFile(8).then(text=>{t8=text;});
+        Promise.all([t1,t2,t3,t4,t5,t6,t7,t8]).then((values)=>{
+            containner.innerText+=values;
+        });
+
         let tl=[];
         for(let i=0;i<8;i++){
             tl.push(getFile(i));
         }
         Promise.all(tl).then((values)=>{
             values.then(text=>{
-                t+=text;
+                containner.innerText+=text;
             });
         });
     }
     
     function fasterFirst(){
-        let t=container3.innerText;
+        let containner=document.getElementById("faster-first");
         let np=new Promise(
             function(){
                 window.setTimeout(function(){
-                    t+="begin...";
+                    containner.innerText+="begin...";
                 },Math.random()*randomTime+baseTime);
             }
         )
@@ -81,49 +98,49 @@ document.querySelector("button").addEventListener("click",function(){
         np.then(
             function(){
                 window.setTimeout(function(){
-                    getFile(1).then(text=>{t+=text;});
+                    getFile(1).then(text=>{containner.innerText+=text;});
                 },Math.random()*randomTime+baseTime);
             }
         ).then(
             function(){
                 window.setTimeout(function(){
-                    getFile(2).then(text=>{t+=text;});
+                    getFile(2).then(text=>{containner.innerText+=text;});
                 },Math.random()*randomTime+baseTime);
             }
         ).then(
             function(){
                 window.setTimeout(function(){
-                    getFile(3).then(text=>{t+=text;});
+                    getFile(3).then(text=>{containner.innerText+=text;});
                 },Math.random()*randomTime+baseTime);
             }
         ).then(
             function(){
                 window.setTimeout(function(){
-                    getFile(4).then(text=>{t+=text;});
+                    getFile(4).then(text=>{containner.innerText+=text;});
                 },Math.random()*randomTime+baseTime);
             }
         ).then(
             function(){
                 window.setTimeout(function(){
-                    getFile(5).then(text=>{t+=text;});
+                    getFile(5).then(text=>{containner.innerText+=text;});
                 },Math.random()*randomTime+baseTime);
             }
         ).then(
             function(){
                 window.setTimeout(function(){
-                    getFile(6).then(text=>{t+=text;});
+                    getFile(6).then(text=>{containner.innerText+=text;});
                 },Math.random()*randomTime+baseTime);
             }
         ).then(
             function(){
                 window.setTimeout(function(){
-                    getFile(7).then(text=>{t+=text;});
+                    getFile(7).then(text=>{containner.innerText+=text;});
                 },Math.random()*randomTime+baseTime);
             }
         ).then(
             function(){
                 window.setTimeout(function(){
-                    getFile(8).then(text=>{t+=text;});
+                    getFile(8).then(text=>{containner.innerText+=text;});
                 },Math.random()*randomTime+baseTime);
             }
         )
