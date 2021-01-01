d8c0fa9e8982d1366db51243cc82d38280f8b478
guojunpei <gjp599@163.com>
2021-01-01 23:25:04 +0800

    fix

--- pages/practice/fetch/fetch.js
+++ pages/practice/fetch/fetch.js
@@ -7,26 +7,6 @@ const container3=document.getElementById("faster-first");
 
 const filesPath="https://guojunpei.github.io/pages/practice/fetch/";
 
-
-/*
-getFile(1).then(text=>{
-    t+=text;
-});
-
-getFile(1).then(text=>{t+=text;});
-
-getFile(1).then(text=>{
-    t=text;
-    console.log(t);
-});
-
-getFile(1).then(text=>{t+=text;});
-
-getFile(1).then(text=>{t=text;});
-*/
-
-
-
 document.querySelector("button").addEventListener("click",function(){
     function getFile(n){
         return fetch(`${filesPath}text${n}.md`).then(response=>response.text());
@@ -156,35 +136,4 @@ document.querySelector("button").addEventListener("click",function(){
     oneByOne();
     afterAll();
     fasterFirst();
-})
-
-/*
-document.getElementById("one-by-one").append()
-document.getElementById("one-by-one").insertAdjacentText
-
-
-fetch("https://github.com/guojunpei/guojunpei.github.io/blob/master/pages/practice/fetch/text4.md").then(r=>{
-    document.getElementById("one-by-one").insertAdjacentText("afterend",r.text())
 });
-
-fetch("https://raw.githubusercontent.com/junjunguo/learning-how-to-learn/main/README.md").then(r=>{
-    let t0=r.text();
-    t0.value
-    console.log(t0);
-    console.log(typeof(t0));
-});
-
-
-
-let t0=fetch("https://raw.githubusercontent.com/junjunguo/learning-how-to-learn/main/README.md").then(r=>{return r.text();
-});
-console.log(t0);
-
-fetch("https://raw.githubusercontent.com/junjunguo/learning-how-to-learn/main/README.md").then(r=>{
-    document.getElementById("one-by-one").insertAdjacentText("afterend",r.text())
-});
-
-fetch("https://github.com/guojunpei/guojunpei.github.io/blob/master/pages/practice/fetch/text4.md").then(r=>{
-    document.getElementById("one-by-one").innerText+=r.text();
-});
-*/
