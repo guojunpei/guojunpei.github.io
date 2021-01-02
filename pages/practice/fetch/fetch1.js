d98ebfd634c6799f6759b723f34c4b4e8f095027
guojunpei <gjp599@163.com>
2021-01-03 00:09:44 +0800

    quick test

--- pages/practice/fetch/fetch1.js
+++ pages/practice/fetch/fetch1.js
@@ -1,8 +1,32 @@
 let baseTime=1000;
 let randomTime=500;
-const delayPromise=(value)=>{
+const filesPath="https://guojunpei.github.io/pages/practice/fetch/";
+function delayPromise(value){
     window.setTimeout(function(){
         return value;
     },Math.random()*randomTime+baseTime);
 }
+
+let urlL=[];
+for(let i=0;i<8;i++){
+    urlL.push(`${filesPath}text${i}`);
+};
+
+const containner1=document.getElementById("one-by-one");
+
+function fillText(div,text){
+    div.innerText+=text;
+}
+
+function getText(url){
+    return fetch(url).then(response=>response.text());
+}
+
+document.querySelector("button").addEventListener("click",()=>{
+    for(let url of urlL){
+        getText(url).then(text=>{
+            delayPromise(fillText(containner1,text));
+        })
+    }
+})
 
