cdedc5396d2f55ae8bc2f67c207e6fae2ee0b3a1
guojunpei <gjp599@163.com>
2021-01-03 00:49:31 +0800

    fix promise

--- pages/practice/fetch/fetch1.js
+++ pages/practice/fetch/fetch1.js
@@ -2,20 +2,22 @@ let baseTime=1000;
 let randomTime=500;
 const filesPath="https://guojunpei.github.io/pages/practice/fetch/";
 function delayPromise(value){
-    window.setTimeout(function(){
-        return value;
-    },Math.random()*randomTime+baseTime);
+    return new Promise((r)=>{
+        window.setTimeout(function(){
+            return r(value);
+        },Math.random()*randomTime+baseTime);
+    })
 }
 
 let urlL=[];
-for(let i=0;i<8;i++){
+for(let i=1;i<9;i++){
     urlL.push(`${filesPath}text${i}`);
 };
 
 const containner1=document.getElementById("one-by-one");
 
 function fillText(div,text){
-    div.innerText+=text;
+    div.innerText+=Date()+text;
 }
 
 function getText(url){
@@ -23,11 +25,21 @@ function getText(url){
 }
 
 function oneByOne(){
+
+    getText(urlL[0]).then(text=>{
+        delayPromise(fillText(containner1,text));
+        getText(urlL[1]);
+    }).then(text=>{
+        delayPromise(fillText(containner1,text));
+    })
+
+    /*
     for(let url of urlL){
         getText(url).then(text=>{
             delayPromise(fillText(containner1,text));
         })
     }
+    */
 }
 
 document.querySelector("button").addEventListener("click",()=>{
