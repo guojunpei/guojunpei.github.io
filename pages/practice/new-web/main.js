5aee53dbcf002a1d9cb94d86625e3c1cd06d3a63
guojunpei <gjp599@163.com>
2021-01-30 23:50:27 +0800

    add js

---
+++ pages/practice/new-web/main.js
@@ -0,0 +1,15 @@
+let profileHead = document.getElementById("profile-head");
+let favouriteHead = document.getElementById("favourite-head");
+let workHead = document.getElementById("work-head");
+let contectHead = document.getElementById("contect-head");
+let profileContent = document.getElementById("profile-content");
+let favouriteContent = document.getElementById("favourite-content");
+let workContent = document.getElementById("work-content");
+let photographyHead = document.getElementById("photography-head");
+let caculatorHead = document.getElementById("caculator-head");
+let NCHead = document.getElementById("NC-head");
+let TTTGHead = document.getElementById("TTTG-head");
+
+profileHead.addEventListener("click",()=>{
+
+})
