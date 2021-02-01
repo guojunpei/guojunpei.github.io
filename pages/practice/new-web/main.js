752b21c9e50e715e0254f9c3737cb709e2f83c50
guojunpei <gjp599@163.com>
2021-02-01 23:53:58 +0800

    add pushSameClassIdIntoList(className) and other

--- pages/practice/new-web/main.js
+++ pages/practice/new-web/main.js
@@ -1,15 +1,25 @@
-let profileHead = document.getElementById("profile-head");
-let favouriteHead = document.getElementById("favourite-head");
-let workHead = document.getElementById("work-head");
-let contectHead = document.getElementById("contect-head");
-let profileContent = document.getElementById("profile-content");
-let favouriteContent = document.getElementById("favourite-content");
-let workContent = document.getElementById("work-content");
-let photographyHead = document.getElementById("photography-head");
-let caculatorHead = document.getElementById("caculator-head");
-let NCHead = document.getElementById("NC-head");
-let TTTGHead = document.getElementById("TTTG-head");
 
-profileHead.addEventListener("click",()=>{
+function pushSameClassIdIntoList(className){
+    let l=[];
+    for(let e of document.querySelectorAll(className)){
+        l.push(e.id)
+    }
+    return l;
+}
 
-})
+let h1L=pushSameClassIdIntoList(".my-h1");
+let h2L=pushSameClassIdIntoList(".my-h2");
+let h3L=pushSameClassIdIntoList(".my-h3");
+
+let c1L=pushSameClassIdIntoList(".my-c1");
+let c2L=pushSameClassIdIntoList(".my-c2");
+let c3L=pushSameClassIdIntoList(".my-c3");
+
+function changecontant(headId){
+    if(h1L.indexOf(headId)>-1){
+        document.getElementById(headId).style.display="flex";
+
+    }
+}
+
+pushSameClassIdIntoList(".my-h3").indexOf("photography-head");
