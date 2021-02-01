2227a57d4b8631166bf42d2ee2f4c9a460debf6f
guojunpei <gjp599@163.com>
2021-02-02 00:17:34 +0800

    fix changecontent(headId)

--- pages/practice/new-web/main.js
+++ pages/practice/new-web/main.js
@@ -17,25 +17,31 @@ let c3L=pushSameClassIdIntoList(".my-c3");
 
 function changecontent(headId){
     if(h1L.indexOf(headId)>-1){
-        document.getElementById(headId).style.display="flex";
         let n=h1L.indexOf(headId);
-        let l=h1L.splice(n);
+        document.getElementById(c1L[n]).style.display="flex";
+        let old=c1l;
+        let l=c1L.splice(n);
         for(let otherId of l){
             document.getElementById(otherId).style.display="none";
         }
+        c1l=old;
     }else if(h2L.indexOf(headId)>-1){
-        document.getElementById(headId).style.display="flex";
         let n=h2L.indexOf(headId);
-        let l=h2L.splice(n);
+        document.getElementById(c2L[n]).style.display="flex";
+        let old=c2l;
+        let l=c2L.splice(n);
         for(let otherId of l){
             document.getElementById(otherId).style.display="none";
         }
+        c2l=old;
     }else if(h3L.indexOf(headId)>-1){
-        document.getElementById(headId).style.display="flex";
         let n=h3L.indexOf(headId);
-        let l=h3L.splice(n);
+        document.getElementById(c3L[n]).style.display="flex";
+        let old=c3l;
+        let l=c3L.splice(n);
         for(let otherId of l){
             document.getElementById(otherId).style.display="none";
         }
+        c3l=old;
     }
 }
