fcad1adba780de0cc89e18b6e167b29902963397
guojunpei <gjp599@163.com>
2021-04-03 23:26:15 +0800

    fix nextimg()

--- pages/practice/new-web/web/work/photograph.js
+++ pages/practice/new-web/web/work/photograph.js
@@ -14,10 +14,26 @@ let img2={
     height:"534px",
 }
 
+let imgL=[img1,img2];
+let imgN="0";
+
+function nextImg(n){
+    imgN=imgN+n;
+    const photo=document.getElementById("photo");
+    if(imgN>imgL.length-1 && imgN>0){
+        imgN=0;}
+    photo.alt=imgL[imgN].alt;
+    photo.src=imgL[imgN].src;
+    photo.with=imgL[imgN].with;
+    photo.height=imgL[imgN].height;
+}
+
+//document.getElementById("photograph").addEventListener("click",nextImg(1));
+
 document.getElementById("photograph").addEventListener("click",()=>{
-    if(imgeSrc=document.getElementById("photo").src==="../images/lake1.jpg"){
-        imgeSrc=document.getElementById("photo").src="../images/lake2.jpg";
+    if(document.getElementById("photo").src==="../images/lake1.jpg"){
+        document.getElementById("photo").src="../images/lake2.jpg";
     }else{
-        imgeSrc=document.getElementById("photo").src="../images/lake1.jpg";
+        document.getElementById("photo").src="../images/lake1.jpg";
     }
 });
