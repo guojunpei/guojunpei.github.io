dc9e9bfe35e716dbec7ef22e84f38f80994393a4
guojunpei <gjp599@163.com>
2021-04-07 23:35:22 +0800

    fix nextimg() in photograph

--- pages/practice/new-web/web/work/photograph.js
+++ pages/practice/new-web/web/work/photograph.js
@@ -2,28 +2,28 @@ let img1={
     alt:"photo",
     name:"lake1",
     src:"../images/lake1.jpg",
-    width:"800px",
-    height:"534px",
+    width:800,
+    height:534,
 }
 
 let img2={
     alt:"photo",
     name:"lake2",
     src:"../images/lake2.jpg",
-    width:"800px",
-    height:"534px",
+    width:800,
+    height:534,
 }
 
 let img3={
     alt:"photo",
     name:"road",
     src:"../images/road.jpg",
-    width:"800px",
-    height:"534px",
+    width:800,
+    height:534,
 }
 
-let imgL=[img1,img2];
-let imgN="0";
+let imgL=[img1,img2,img3];
+let imgN=0;
 
 window.addEventListener("resize",()=>{
     const eleWidth=document.getElementById("photograph").clientWidth;
@@ -34,20 +34,26 @@ window.addEventListener("resize",()=>{
     }
 });
 
-function nextImg(n){
-    imgN=imgN+n;
+function nextImg(){
+    imgN=imgN+1;
+    console.log("imgN:"+imgN)
     const photo=document.getElementById("photo");
     if(imgN>imgL.length-1 && imgN>0){
         imgN=0;}
+    console.log(imgL[imgN]);
     photo.alt=imgL[imgN].alt;
     photo.src=imgL[imgN].src;
     photo.width=imgL[imgN].width;
     photo.height=imgL[imgN].height;
+    console.log(photo.alt);
+    console.log(photo.src);
+    console.log(photo.width);
+    console.log(photo.height);
 }
 
-//document.getElementById("photograph").addEventListener("click",nextImg(1));
+document.getElementById("photograph").addEventListener("click",nextImg);
 
-
+/*
 document.getElementById("photograph").addEventListener("click",()=>{
     if(document.getElementById("photo").src==="../images/lake1.jpg"){
         document.getElementById("photo").src="../images/lake2.jpg";
@@ -59,3 +65,4 @@ document.getElementById("photograph").addEventListener("click",()=>{
         }
     }
 });
+*/
