f97ab5ae716d0f8ab08b7598bc6f2c24e2f2a707
guojunpei <gjp599@163.com>
2021-04-08 23:37:25 +0800

    add imgResize() to photograph in new web

--- pages/practice/new-web/web/work/photograph.js
+++ pages/practice/new-web/web/work/photograph.js
@@ -25,19 +25,27 @@ let img3={
 let imgL=[img1,img2,img3];
 let imgN=0;
 
+const photo=document.getElementById("photo");
+
+function imgResize(){
+    const imgPrecent= photo.clientWidth/photo.width;
+    //photo.width=imgL[imgN].width;
+    photo.height=imgL[imgN].height*imgPrecent;
+}
+
 window.addEventListener("resize",()=>{
     const eleWidth=document.getElementById("photograph").clientWidth;
-    const wrapWidth=document.getElementById("content-containner").clientWidth
-    const headWidth=document.getElementById("inner-heads-containner").clientWidth
+    const wrapWidth=document.getElementById("content-containner").clientWidth;
+    const headWidth=document.getElementById("inner-heads-containner").clientWidth;
     if(eleWidth>wrapWidth-headWidth){
         document.getElementById("photo").Width=wrapWidth-headWidth;
     }
+    imgResize();
 });
 
 function nextImg(){
     imgN=imgN+1;
     console.log("imgN:"+imgN)
-    const photo=document.getElementById("photo");
     if(imgN>imgL.length-1 && imgN>0){
         imgN=0;}
     console.log(imgL[imgN]);
@@ -52,17 +60,3 @@ function nextImg(){
 }
 
 document.getElementById("photograph").addEventListener("click",nextImg);
-
-/*
-document.getElementById("photograph").addEventListener("click",()=>{
-    if(document.getElementById("photo").src==="../images/lake1.jpg"){
-        document.getElementById("photo").src="../images/lake2.jpg";
-    }else{
-        if(document.getElementById("photo").src==="../images/lake2.jpg"){
-            document.getElementById("photo").src="../images/road.jpg";
-        }else{
-            document.getElementById("photo").src="../images/lake1.jpg";
-        }
-    }
-});
-*/
