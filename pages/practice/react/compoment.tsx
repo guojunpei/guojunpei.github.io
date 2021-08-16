18e1a6b60b191a9d76ce916ea1f96d012f8c1d5b
guojunpei <gjp599@163.com>
2021-08-16 23:26:07 +0800

    practice react component and try to building

---
+++ pages/practice/react/compoment.tsx
@@ -0,0 +1,19 @@
+/*
+import React from 'react';
+import { render } from 'react-dom';
+
+class shoppingList extends React.Component{
+    render(){
+        return(
+            <div className="shopping-list">
+                <h1>shopping List for {this.props.name}</h1>
+                <ul>
+                    <li>Instagram</li>
+                    <li>WhatsApp</li>
+                    <li>Oculus</li>
+                </ul>
+            </div>
+        );
+    }
+}
+*/
